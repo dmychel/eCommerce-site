ce66f6b19ac86d93a6a7a2b464145a7cb328f254
dmychel <dmychel@tutanota.com>
2024-03-07 17:48:02 -0500

    created comp

---
+++ src/routes/Error.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <>
+      <h2>Error 404</h2>
+      <p>
+        It seems this page doesn't exist. <Link to="/">Click Here</Link> to
+        navigate back to the home page.
+      </p>
+    </>
+  );
+};
+
+export default Error;
