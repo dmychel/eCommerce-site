9e6877f1ff66b1d50adb76a21c0cc5dc4cbce899
dmychel <dmychel@tutanota.com>
2024-03-07 17:36:33 -0500

    changed index element to component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./routes/Index.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Index />,
   },
 ]);
 
