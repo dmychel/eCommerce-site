43dd869928d3f1653f30c827a6a707113e095dda
dmychel <dmychel@tutanota.com>
2024-03-08 00:16:27 -0500

    created mouse over mouse out functions to emulate a hover effect

--- src/components/product/ProductChild.jsx
+++ src/components/product/ProductChild.jsx
@@ -2,11 +2,22 @@ import styles from '/src/styles/product.module.scss'
 import PropTypes from 'prop-types'
 
 const ProductChild = ({ product }) => {
+    const mouseOver = (id) => {
+        const img = document.getElementById(id)
+        img.src = product.preview2
+    }
+
+    const mouseOut = (id) => {
+        const img = document.getElementById(id)
+        img.src = product.preview
+    }
+
+
     return (
 
-        <div className={styles.product}>
+        <div className={styles.product} onMouseOver={() => mouseOver(product.id)} onMouseOut={() => mouseOut(product.id)} >
             <div className={styles.productImage}>
-                <img src={product.preview} />
+                <img src={product.preview} id={product.id} />
             </div>
             <div className={styles.productDesc}>
                 <h4>{product.title}</h4>
@@ -19,6 +30,7 @@ const ProductChild = ({ product }) => {
 
 ProductChild.propTypes = {
     product: PropTypes.object,
+
 }
 
 export default ProductChild
