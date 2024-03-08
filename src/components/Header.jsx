89d6e5d706618351a742fe826c8e787e06243a24
dmychel <dmychel@tutanota.com>
2024-03-08 17:14:04 -0500

    removed link path temporarily

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <Link to="/"><h1>Onitsuka Tiger</h1></Link>
+      <Link to="/">
+        <h1>Onitsuka Tiger</h1>
+      </Link>
       <nav>
-        <Link to='/shop'>shop</Link>
-        <Link to='/cart' className={styles.cart}><img src="/assets/imgs/icons/cart.png" alt="" /></Link>
+        <Link to="/shop">shop</Link>
+        <Link className={styles.cart}>
+          <img src="/assets/imgs/icons/cart.png" alt="" />
+        </Link>
       </nav>
     </header>
   );
