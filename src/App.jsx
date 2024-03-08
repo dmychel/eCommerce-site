f3a75aaebaf4104ccc30e8fe6c5608d982ea8cd5
dmychel <dmychel@tutanota.com>
2024-03-07 19:35:47 -0500

    moved all routing logic to this file so I can pass props to make the app functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import "./App.css";
 import Index from "./routes/Index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// routes
+import Cart from "./routes/Cart.jsx";
+import Shop from "./routes/Shop.jsx";
+import Error from "./routes/Error.jsx";
 
 function App() {
   return (
-    <section className="app">
-      <div className="header">
+    <BrowserRouter>
+      <header>
         <h1>Onitsuka Tiger</h1>
         <nav>
           <span>shop</span>
           <span>cart</span>
         </nav>
-      </div>
-      <Index />
-    </section>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
