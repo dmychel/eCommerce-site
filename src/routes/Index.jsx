0ed703f2f309d6bab87f236a29f64ca474220cfe
dmychel <dmychel@tutanota.com>
2024-03-07 19:15:50 -0500

    created jsx

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,9 +2,43 @@ import styles from "/src/styles/index.module.scss";
 
 const Index = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <section className={styles.home}>
+      <div className={styles.sub}>
+        <div className={styles.text}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={styles.image}>
+          <img src="/assets/imgs/sub-1.webp" alt="" />
+        </div>
+      </div>
+
+      <div className={`${styles.sub} ${styles.sub2}`}>
+        <div className={styles.text}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={styles.image}>
+          <img src="/assets/imgs/sub-2.webp" alt="" />
+        </div>
+      </div>
+
+      <div className={styles.sub}>
+        <div className={styles.text}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={styles.image}>
+          <img src="/assets/imgs/sub-3.webp" alt="" />
+        </div>
+      </div>
+
+      <div className={`${styles.sub} ${styles.sub2}`}>
+        <div className={styles.text}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className={styles.image}>
+          <img src="/assets/imgs/sub-4.webp" alt="" />
+        </div>
+      </div>
+    </section>
   );
 };
 
