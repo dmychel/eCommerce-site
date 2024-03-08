198c5c4d63a3e572835773342784a78309c5d4d9
dmychel <dmychel@tutanota.com>
2024-03-07 19:15:38 -0500

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import "./App.css";
 import Index from "./routes/Index";
 
 function App() {
-  return <Index />;
+  return (
+    <section className="app">
+      <div className="header">
+        <h1>Onitsuka Tiger</h1>
+        <nav>
+          <span>shop</span>
+          <span>cart</span>
+        </nav>
+      </div>
+      <Index />
+    </section>
+  );
 }
 
 export default App;
