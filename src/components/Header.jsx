1fe09b7ff7b210567d286124c99c6324c73a6df1
dmychel <dmychel@tutanota.com>
2024-03-08 09:26:24 -0500

    changed spans to links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
     <header>
       <Link to="/"><h1>Onitsuka Tiger</h1></Link>
       <nav>
-        <span>shop</span>
-        <span>cart</span>
+        <Link to='/shop'>shop</Link>
+        <Link to='/cart' className={styles.cart}><img src="/assets/imgs/icons/cart.png" alt="" /></Link>
       </nav>
     </header>
   );
