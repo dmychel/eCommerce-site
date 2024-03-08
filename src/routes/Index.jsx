d0c609eec5624eef460eeefa5135c34c0f6adc4b
dmychel <dmychel@tutanota.com>
2024-03-07 20:17:49 -0500

    changed jsx

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,11 +1,14 @@
 import styles from "/src/styles/index.module.scss";
+import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
     <section className={styles.home}>
       <div className={styles.sub}>
         <div className={styles.text}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <Link to="/shop" className={styles.button}>
+            SHOP NOW
+          </Link>
         </div>
         <div className={styles.image}>
           <img src="/assets/imgs/sub-1.webp" alt="" />
