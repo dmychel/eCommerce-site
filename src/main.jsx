9dc088324a7d03b571368ad04d748da50bd2f364
dmychel <dmychel@tutanota.com>
2024-03-07 19:36:01 -0500

    migrated routes to app jsx for prop functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,9 @@
-// react
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// react router
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// routes
 import App from "./App.jsx";
-import Cart from "./routes/Cart.jsx";
-import Error from "./routes/Error.jsx";
-import Shop from "./routes/Shop.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/shop",
-    element: <Shop />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
