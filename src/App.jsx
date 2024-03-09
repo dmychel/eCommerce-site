667cfdfc26efef56c3fcaf9d1f1af4c74739eade
dmychel <dmychel@tutanota.com>
2024-03-08 23:59:43 -0500

    Created add to cart logic
    handle submit function creayes a new object with the added property of value.
    
    parseProduct:
    - detects whether the product's quantity is less than 1, or greater than 1.
    - less than the function is returned
    - greater than 1 an array is set equal to the result of duplicateProduct function, then add to cart function is returned.
    - else the addToCart function is returned
    
    duplicateProduct:
    - a for loop is run dependent upon the product quantity value.
    - each loop duplicates the product, sets it's quantity to 1, given a random ID, then pushes to an array.
    - the array is returned
    
    addToCart:
    - if statement checks for instance of an array
        - if true the array is mapped and the setCart function is run to add the items into useState
    - else the setCart function is run to add item into useState

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,60 @@ import Shop from "./routes/Shop.jsx";
 import Error from "./routes/Error.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { useState } from "react";
 
 
 
 function App() {
+  const [cart, setCart] = useState([])
+
   const handleSubmit = (e, product, quantity) => {
     e.preventDefault()
-    console.log('submit')
-    console.log(product)
-    console.log(quantity)
+
+    const newProduct = {
+      title: product.title,
+      model: product.model,
+      price: product.price,
+      quantity: quantity,
+      id: product.id
+    }
+    return parseProduct(newProduct)
   }
+
+  const parseProduct = (product) => {
+    if (product.quantity < 1) {
+      return
+    }
+    else if (product.quantity > 1) {
+      const arr = duplicateProduct(product)
+      return addToCart(arr)
+    }
+    else { return addToCart(product) }
+  }
+
+  const duplicateProduct = (product) => {
+    let arr = []
+    for (let i = 0; i < product.quantity; i++) {
+      const dupProduct = { ...product }
+
+      dupProduct.quantity = 1;
+      dupProduct.id = crypto.randomUUID();
+
+      arr.push(dupProduct);
+    }
+    return arr
+  }
+
+  const addToCart = (product) => {
+    if (product instanceof Array) {
+      product.map(((item) => {
+        return setCart((currentCart) => [...currentCart, item])
+      }))
+    }
+    else {
+      return setCart([...cart, product])
+    }
+  };
 
   return (
     <BrowserRouter>
