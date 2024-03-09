51426182a03c5531bb204b598b78fc0464c67ec2
dmychel <dmychel@tutanota.com>
2024-03-08 23:20:01 -0500

    moved quantity useState to this file

--- src/components/product/ProductChild.jsx
+++ src/components/product/ProductChild.jsx
@@ -1,7 +1,10 @@
 import styles from '/src/styles/product.module.scss'
 import PropTypes from 'prop-types'
 
-const ProductChild = ({ product, handleSubmit, quantity, setQuantity }) => {
+import { useState } from 'react';
+
+const ProductChild = ({ product, handleSubmit }) => {
+    const [quantity, setQuantity] = useState(0);
     const mouseOver = (id) => {
         const img = document.getElementById(id)
         img.src = product.preview2
@@ -11,7 +14,6 @@ const ProductChild = ({ product, handleSubmit, quantity, setQuantity }) => {
         const img = document.getElementById(id)
         img.src = product.preview
     }
-
 
     return (
         <div className={styles.product} onMouseOver={() => mouseOver(product.id)} onMouseOut={() => mouseOut(product.id)} >
@@ -23,8 +25,8 @@ const ProductChild = ({ product, handleSubmit, quantity, setQuantity }) => {
                 <p>{product.model}</p>
                 <span>{product.price}</span>
             </div>
-            <form>
-                <input type="number" min={0} max={99} />
+            <form onSubmit={handleSubmit}>
+                <input type="number" min={0} max={99} value={quantity} onChange={(e) => setQuantity(e.target.value)} />
                 <input type="submit" value='&#9989;' />
             </form>
         </div>
@@ -34,8 +36,6 @@ const ProductChild = ({ product, handleSubmit, quantity, setQuantity }) => {
 ProductChild.propTypes = {
     product: PropTypes.object,
     handleSubmit: PropTypes.func,
-    quantity: PropTypes.number,
-    setQuantity: PropTypes.func,
 }
 
 export default ProductChild
