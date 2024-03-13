2512fbc8f18ef8f69c7dd1913b834dbcd9503c3e
dmychel <dmychel@tutanota.com>
2024-03-13 19:00:29 -0400

    created component

---
+++ src/components/SubRender.jsx
@@ -0,0 +1,14 @@
+const SubRender = () => {
+  return (
+    <section>
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <img src="" />
+      </div>
+    </section>
+  );
+};
+
+export default SubRender;
