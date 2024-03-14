61bf300e09a971d23a8c5103c3ca001e7f1ecd43
dmychel <dmychel@tutanota.com>
2024-03-13 20:42:46 -0400

    removed jsx and replaced with components

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,10 +1,11 @@
+import SubRender from "../components/SubRender";
 import styles from "/src/styles/index.module.scss";
 import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
-    <section className={styles.home}>
-      <div className={styles.sub}>
+    <div className={styles.home}>
+      <section className={styles.sub}>
         <div className={styles.text}>
           <Link to="/shop" className={styles.button}>
             SHOP NOW
@@ -13,9 +14,33 @@ const Index = () => {
         <div className={styles.image}>
           <img src="/assets/imgs/sub/sub-1.webp" alt="" />
         </div>
-      </div>
+      </section>
 
-      <div className={`${styles.sub} ${styles.sub2}`}>
+      <SubRender
+        type={true}
+        text={`Reborn & Reimagined.`}
+        altText={`Onitsuka Tiger grew to become ASICS, and go on to establish itself as one of the world's leading sports brands.`}
+        img={`/assets/imgs/sub/sub-2.webp`}
+        altImg={`/assets/imgs/sub/tales8.webp`}
+      />
+
+      <SubRender
+        type={false}
+        text={`Reborn & Reimagined.`}
+        altText={`Onitsuka Tiger grew to become ASICS, and go on to establish itself as one of the world's leading sports brands.`}
+        img={`/assets/imgs/sub/sub-2.webp`}
+        altImg={`/assets/imgs/sub/tales8.webp`}
+      />
+
+      <SubRender
+        type={true}
+        text={`Reborn & Reimagined.`}
+        altText={`Onitsuka Tiger grew to become ASICS, and go on to establish itself as one of the world's leading sports brands.`}
+        img={`/assets/imgs/sub/sub-2.webp`}
+        altImg={`/assets/imgs/sub/tales8.webp`}
+      />
+
+      {/* <div className={`${styles.sub} ${styles.sub2}`}>
         <div className={styles.text}>
           <p>THE BIRTH OF AN ICON</p>
         </div>
@@ -40,8 +65,8 @@ const Index = () => {
         <div className={styles.image}>
           <img src="/assets/imgs/sub/sub-4.webp" alt="" />
         </div>
-      </div>
-    </section>
+      </div> */}
+    </div>
   );
 };
 
