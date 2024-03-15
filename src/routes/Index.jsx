0a02152cf629966bc25642959b770baf91880e8a
dmychel <dmychel@tutanota.com>
2024-03-14 20:20:05 -0400

    removed comments

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -39,33 +39,6 @@ const Index = () => {
         img={`/assets/imgs/sub/sub-2.webp`}
         altImg={`/assets/imgs/sub/tales8.webp`}
       />
-
-      {/* <div className={`${styles.sub} ${styles.sub2}`}>
-        <div className={styles.text}>
-          <p>THE BIRTH OF AN ICON</p>
-        </div>
-        <div className={styles.image}>
-          <img src="/assets/imgs/sub/sub-2.webp" alt="" />
-        </div>
-      </div>
-
-      <div className={styles.sub}>
-        <div className={styles.text}>
-          <p>AN ATHLETIC FOREFRUNNER</p>
-        </div>
-        <div className={styles.image}>
-          <img src="/assets/imgs/sub/sub-3.webp" alt="" />
-        </div>
-      </div>
-
-      <div className={`${styles.sub} ${styles.sub2}`}>
-        <div className={styles.text}>
-          <p>BORN FROM AMBITION</p>
-        </div>
-        <div className={styles.image}>
-          <img src="/assets/imgs/sub/sub-4.webp" alt="" />
-        </div>
-      </div> */}
     </div>
   );
 };
